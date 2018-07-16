040127a601f198628b6c38081237400be63e1616
MohamedTarek <30350416+M7mdVI@users.noreply.github.com>
2018-07-16 10:08:51 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,29 +1,7 @@
-
-const Discord = require("discord.js");
-const client = new Discord.Client();
-const fs = require("fs");
-var Canvas = require('canvas')
-const ms = require("ms");
-var jimp = require('jimp')
-const db = require('quick.db')
-const arraySort = require('array-sort'),
-      table = require('table');
-const translate = require('google-translate-api');
-const moment = require('moment');
-const ytdl = require('ytdl-core');
-const Codes = require('codes-official');
-const request = require('request');
-const getYoutubeID = require('get-youtube-id');
-const fetchVideoInfo = require('youtube-info');
-const math = require('math-expression-evaluator');
-const stripIndents = require('common-tags').stripIndents;
-const pretty = require('pretty-ms');
-const rn = require('random-number');
-let done = {};
-const child_process = require("child_process");
 const adminprefix = "-";
 const devs = ['389755149939113987'];
 var prefix = '-';
+
 
 
 client.on('ready', () => {
